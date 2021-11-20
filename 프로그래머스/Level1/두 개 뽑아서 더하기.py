38417534d7ca5cb3801ebf21a0e684b667d56505
Yongjin Kim <dydwls0669@gmail.com>
2021-11-21 00:59:49 +0900

    구현

---
+++ 프로그래머스/Level1/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68644
+
+from itertools import combinations
+
+def solution(numbers):
+    answer = set()
+    for a, b in list(combinations(numbers, 2)):
+        answer.add(a+b)
+    answer = list(answer)
+    answer.sort()
+    return answer
