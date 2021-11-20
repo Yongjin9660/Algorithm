6b8e39a6598c2562c945af294d19dee66dfa18e9
Yongjin Kim <dydwls0669@gmail.com>
2021-11-20 15:53:23 +0900

    formatting 수정

--- 프로그래머스/Level1/2016년.py
+++ 프로그래머스/Level1/2016년.py
@@ -1,6 +1,6 @@
 def solution(a, b):
-  months = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-  days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
-  total_days = sum(months[1 : a]) + b - 1
-  answer = days[total_days % 7]
-  return answer
+    months = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    total_days = sum(months[1 : a]) + b - 1
+    answer = days[total_days % 7]
+    return answer
