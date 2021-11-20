31f8221457325d68c44de768e0cf561a4d2dafa6
Yongjin Kim <dydwls0669@gmail.com>
2021-11-20 15:54:03 +0900

    url 추가

--- 프로그래머스/Level1/2016년.py
+++ 프로그래머스/Level1/2016년.py
@@ -1,3 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12901
+
 def solution(a, b):
     months = [0,31,29,31,30,31,30,31,31,30,31,30,31]
     days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
