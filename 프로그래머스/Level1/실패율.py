c63aab61948f5b1785e175e967a075e4ffb7decf
Yongjin Kim <dydwls0669@gmail.com>
2021-11-22 23:08:01 +0900

    구현

---
+++ 프로그래머스/Level1/실패율.py
@@ -0,0 +1,18 @@
+# 실패율
+
+def solution(N, stages):
+    temp = []
+    len_stages = len(stages)
+    for i in range(1, N + 1):
+        cnt = stages.count(i)
+        if cnt == 0:
+            fail = 0
+        else:
+            fail = cnt / len_stages
+        temp.append((i, fail))
+        len_stages -= cnt    
+    temp.sort(key=lambda x:x[1], reverse=True)
+    answer = []
+    for a, b in temp:
+        answer.append(a)
+    return answer
