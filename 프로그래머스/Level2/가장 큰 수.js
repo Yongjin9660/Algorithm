93a06210fa2eff9ffe0f08d11c13420a07b16015
Yongjin Kim <dydwls0669@gmail.com>
2021-12-11 13:05:29 +0900

    정렬

---
+++ 프로그래머스/Level2/가장 큰 수.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42746
+
+function solution(numbers) {
+  const answer = numbers
+    .map((n) => n + '')
+    .sort((a, b) => b + a - (a + b))
+    .join('');
+  return answer[0] === '0' ? '0' : answer;
+}
