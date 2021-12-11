59e5ec045b906fb2b62c2a3977c6eb4577f5a153
yongjin <dydwls0669@gmail.com>
2021-12-11 20:36:48 +0900

    정렬

---
+++ 프로그래머스/Level2/H-Index.js
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42747
+
+function solution(citations) {
+  citations = citations.sort((a, b) => b - a);
+  let i = 0;
+  while (i + 1 <= citations[i]) {
+    i++;
+  }
+  return i;
+}
