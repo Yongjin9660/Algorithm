7702de0aac68a01b16383abd6e1c54e406ccd0bc
yongjin <dydwls0669@gmail.com>
2021-12-13 19:31:40 +0900

    조합

---
+++ 프로그래머스/Level1/두 개 뽑아서 더하기.js
@@ -0,0 +1,26 @@
+// https://programmers.co.kr/learn/courses/30/lessons/68644
+
+const combinations = (arr, n) => {
+	const result = [];
+	if (n === 1) return arr.map((v) => [v]);
+	arr.forEach((a, idx) => {
+		const fix = a;
+		const restArr = arr.slice(idx + 1);
+		const combinationArr = combinations(restArr, n - 1);
+		const combineFix = combinationArr.map((arr) => [...arr, fix]);
+		result.push(...combineFix);
+	});
+	return result;
+};
+
+const solution = (numbers) => {
+	const numberSet = new Set();
+	const sumArr = combinations(numbers, 2)
+		.map((arr) => arr.reduce((prev, cur) => prev + cur, 0))
+		.forEach((s) => numberSet.add(s));
+	const answer = [];
+	for (let a of numberSet) {
+		answer.push(a);
+	}
+	return answer.sort((a, b) => a - b);
+};
