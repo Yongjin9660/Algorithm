8c6c95b26deeac79191899a0c790957237ea9faa
yongjin <dydwls0669@gmail.com>
2021-12-14 14:44:35 +0900

    수학

---
+++ 프로그래머스/Level2/N개의 최소공배수.js
@@ -0,0 +1,22 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12953
+
+const gcd = (a, b) => {
+	while (b > 0) {
+		let temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+};
+
+const lcm = (a, b) => {
+	return (a * b) / gcd(a, b);
+};
+
+const solution = (arr) => {
+	let answer = 1;
+	for (let i = 0; i < arr.length; i++) {
+		answer = lcm(answer, arr[i]);
+	}
+	return answer;
+};
