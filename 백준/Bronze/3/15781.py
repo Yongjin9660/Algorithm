a6033fd42d11266b492f2fe5aa8061333a018212
yongjin <dydwls0669@gmail.com>
2021-12-20 12:50:59 +0900

    구현

---
+++ 백준/Bronze/3/15781.py
@@ -0,0 +1,7 @@
+# 헬맷과 조끼
+# https://www.acmicpc.net/problem/15781
+
+n, m = map(int, input().split(' '))
+h = max(list(map(int, input().split(' '))))
+j = max(list(map(int, input().split(' '))))
+print(h + j)
