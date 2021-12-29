72323354f406ba0f6b650295fc99b3661a9f236e
yongjin <dydwls0669@gmail.com>
2021-12-29 10:49:33 +0900

    dp

---
+++ 백준/Silver/3/1904.py
@@ -0,0 +1,13 @@
+# 01타일
+# https://www.acmicpc.net/problem/1904
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0, 1, 2] + [0] * (N - 2)
+for i in range(3, N+1):
+  dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[N])
